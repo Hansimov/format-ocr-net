6cb04e7628b98f321347b47077d12effd3959b17
Hansimov <591172499@qq.com>
2024-02-01 17:29:41 +0800

    :zap: [Enhance] SimpleEquationsDatasetGenerator: Support random symbols

--- mocks/simple_equations_dataset_generator.py
+++ mocks/simple_equations_dataset_generator.py
@@ -1,5 +1,6 @@
 import random
 import re
+import string
 import sympy as sp
 
 from documents.filepath_converter import FilepathConverter
@@ -13,18 +14,28 @@ class SimpleEquationsDatasetGenerator:
             root="datasets", parent=self.dataset_name, ext=".txt"
         )
 
+    def random_symbol(self, max_len=2):
+        # generate a symbol of [a-zA-Z0-9]+, with length <= max_len
+        symbol_str = "".join(random.choices(string.ascii_letters))
+        symbol_str += "".join(
+            random.choices(
+                string.ascii_letters + string.digits, k=random.randint(1, max_len - 1)
+            )
+        )
+        symbol = sp.symbols(symbol_str)
+        return symbol
+
     def generate(self):
-        x, y, z = sp.symbols("x y z")
         ops = [sp.Add, sp.Mul, sp.Pow, sp.sqrt, sp.sin, sp.cos]
         equations_num = 20
         for i in range(equations_num):
             random.seed(i)
             # choose random number of ops and symbols, then combine all to single equation
             num_ops = random.randint(2, 5)
-            equation = x
+            equation = self.random_symbol()
             for _ in range(num_ops):
                 op = random.choice(ops)
-                symbol = random.choice([x, y, z])
+                symbol = self.random_symbol()
                 try:
                     equation = op(equation, symbol)
                 except:
@@ -38,7 +49,7 @@ class SimpleEquationsDatasetGenerator:
             latex_str = re.sub(r"\s+", " ", latex_str)
             logger.success(latex_str)
             filepath = self.filepath_converter.hash(latex_str)
-            print(filepath)
+            print(filepath.name)
 
 
 if __name__ == "__main__":
