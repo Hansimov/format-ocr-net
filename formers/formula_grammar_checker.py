dff01a73892b80b7712390fa0d414a2a90491bbb
Hansimov <591172499@qq.com>
2024-02-07 11:39:08 +0800

    :zap: [Enhance] Use LALR parser, ignore WS, reduce eqn_with_delim

--- formers/formula_grammar_checker.py
+++ formers/formula_grammar_checker.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from lark import Lark
+from utils.logger import logger
 
 
 class FormulaGrammarChecker:
@@ -12,22 +13,35 @@ class FormulaGrammarChecker:
             # escape the backslash (\) in .lark file
             grammar = rf.read().replace("\\", "\\\\")
 
-        self.parser = Lark(grammar, start="full_eqn")
+        self.parser = Lark(grammar, start="full_eqn", parser="lalr")
 
-    def parse(self, expr):
+    def check(self, expr, verbose=False):
         try:
             res = self.parser.parse(expr)
-            print(res.pretty())
-            print(f"Valid: {expr}")
+            if verbose:
+                print(res.pretty())
+                logger.success(f"Valid: {expr}")
         except Exception as e:
             print(e)
-            print(f"Invalid: {expr}")
+            logger.warn(f"Invalid: {expr}")
+            raise e
 
 
 if __name__ == "__main__":
-    # expr = r"\frac{ab}{cd} + \frac{ef}{gh} = \bar{x} + \bar{b}"
-    # expr = r"{ a b }"
-    # expr = r"G ( 0, 1 )"
-    expr = r"G ( 0 , t ^ { \prime \prime } ; 0 , t ^ { \prime \prime } ) = i \int \int \frac { d \omega ^ { \prime \prime } } { 2 \pi } \frac { d k ^ { \prime \prime } } { 2 \pi } P _ { 0 } ( \omega ^ { \prime \prime } , k ^ { \prime \prime } )"
+    exprs = [
+        # r"{ a b c } ",
+        # r"G ( 0, 1 ) H ( 2, 3 )",
+        # r"\bar{x}",
+        # r"\bar{x y}",
+        # r"\frac{a}{b}",
+        # r"\frac ab",
+        # r"\frac a b c",
+        # r"\frac{ab}{cd}",
+        # r"\frac{ab}{cd} + \frac{ef}{gh} = \bar{x} + \bar{b}",
+        # r"G ( 0 , t ^ { \prime \prime } ; 0 , t ^ { \prime \prime } ) = i \int \int \frac { d \omega ^ { \prime \prime } } { 2 \pi } \frac { d k ^ { \prime \prime } } { 2 \pi } P _ { 0 } ( \omega ^ { \prime \prime } , k ^ { \prime \prime } )",
+        r"{ \frac { \phi ^ { \prime \prime } } { A } } + { \frac { 1 } { A } } \left( - { \frac { 1 } { 2 } } { \frac { A ^ { \prime } } { A } } + 2 { \frac { B ^ { \prime } } { B } } + { \frac { 2 } { r } } \right) \phi ^ { \prime } - { \frac { 2 } { r ^ { 2 } } } \phi - \lambda \phi ( \phi ^ { 2 } - \eta ^ { 2 } ) = 0 \, .",
+        # r"E _ { A D M } = \frac { 1 } { 1 6 \pi G _ { 1 0 } } \oint _ { \infty } d \Sigma ^ { m } \lbrace { } ^ { \circ } D _ { n } g _ { m p } - { } ^ { \circ } D _ { m } g _ { n p } \rbrace { } ^ { \circ } g ^ { n p }",
+    ]
     checker = FormulaGrammarChecker()
-    checker.parse(expr)
+    for expr in exprs:
+        checker.check(expr, verbose=True)
